1042f94a4286900364411759af21b38d3acfeb86
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-06-25 13:23:19 +0530

    recipe model created

---
+++ lib/domain/recipe_model/recipe.dart
@@ -0,0 +1,28 @@
+import 'package:hive/hive.dart';
+part 'recipe.g.dart';
+
+@HiveType(typeId: 1)
+class Recipe {
+  @HiveField(0)
+  String image;
+  @HiveField(1)
+  String name;
+  @HiveField(2)
+  String duration;
+  @HiveField(3)
+  String category;
+  @HiveField(4)
+  String ingrediants;
+  @HiveField(5)
+  String procedure;
+  @HiveField(6)
+  String videoLink;
+  Recipe(
+      {required this.image,
+      required this.name,
+      required this.duration,
+      required this.category,
+      required this.ingrediants,
+      required this.procedure,
+      required this.videoLink});
+}
---
+++ lib/domain/recipe_model/recipe.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recipe.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class RecipeAdapter extends TypeAdapter<Recipe> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Recipe read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Recipe(
+      image: fields[0] as String,
+      name: fields[1] as String,
+      duration: fields[2] as String,
+      category: fields[3] as String,
+      ingrediants: fields[4] as String,
+      procedure: fields[5] as String,
+      videoLink: fields[6] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Recipe obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.image)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.duration)
+      ..writeByte(3)
+      ..write(obj.category)
+      ..writeByte(4)
+      ..write(obj.ingrediants)
+      ..writeByte(5)
+      ..write(obj.procedure)
+      ..writeByte(6)
+      ..write(obj.videoLink);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is RecipeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
